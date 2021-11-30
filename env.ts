a49abb064b7019a13a7bc637d7bb9cd26a8f71d1
Alanakarine-17 <alanacantuaria@gmail.com>
2021-11-30 14:54:24 -0300

    commit

--- env.ts
+++ env.ts
@@ -21,7 +21,7 @@ export default Env.rules({
 	APP_NAME: Env.schema.string(),
   	DRIVE_DISK: Env.schema.enum(['local'] as const),
 	NODE_ENV: Env.schema.enum(['development', 'production', 'testing'] as const),
-	DB_CONECTION: Env.schema.string(),
+	//DB_CONECTION: Env.schema.string(),
 	//MYSQL_HOST: Env.schema.string ({format: 'host'}),
   	//MYSQL_PORT: Env.schema.number (),
     //MYSQL_USER: Env.schema.string (),
