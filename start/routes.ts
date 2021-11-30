2d1f08718f870d97deb8f892233163bd9d5352e1
Alanakarine-17 <alanacantuaria@gmail.com>
2021-11-30 14:00:29 -0300

    commit

--- start/routes.ts
+++ start/routes.ts
@@ -19,10 +19,10 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route'
-//
-//Route.get('/', async () => {
-//  return { hello: 'world' }
-//})
+
+Route.get('/', async () => {
+  return { hello: 'world' }
+})
 
 Route.post("/register", "AuthController.register")
 Route.post("/login", "AuthController.login")
